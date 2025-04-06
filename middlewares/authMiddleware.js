45968f47631b6b3619b4ffb20aa1876546355849
antony190599 <antonycayo19@gmail.com>
2025-04-05 21:17:31 -0500

    actualizacion-final

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,12 @@
 export const authMiddleware = (req, res, next) => {
+    // Bypass authentication for the /docs route
+    if (req.path.startsWith('/docs')) {
+        return next();
+    }
   const apiKey = req.headers['x-api-key'];
   const validApiKey = process.env.API_KEY;
   console.log(`API Key: ${apiKey}`);
-  
+
 
   if (!apiKey || apiKey !== validApiKey) {
     return res.status(401).json({ error: 'Unauthorized: Invalid API Key' });
